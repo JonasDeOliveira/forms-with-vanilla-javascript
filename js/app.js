6aa5b5fd7baf942a3f0b7147f76e9bd5c8f48cb8
Jonas Oliveira <jloide.essence@rd.com.br>
2021-08-10 10:38:47 -0300

    aulas form js

---
+++ js/app.js
@@ -0,0 +1,131 @@
+/******** 
+ * script sobre formulário com javascript
+ * todos os console.log desse arquivo estão comentados
+ * para testar, encontre a parte que deseja estudar e descomente o console.log desejado
+ * se necessário, adicione novas linhas de código
+ * *******/
+
+//====inicio contator usando input hidden====
+let btnMenos = document.querySelector('.btn-menos')
+let btnMais = document.querySelector('.btn-mais')
+let textoQtd = document.querySelector('.qtd')
+let qtd = textoQtd.textContent
+let qtdHidden = document.querySelector('#quantidade')
+
+function aumentar() {
+    qtd++
+    textoQtd.textContent = qtd
+    qtdHidden.value = qtd
+}
+
+function diminuir() {
+    if (qtd == 1) {
+        alert('quantidade minima é 1')
+        return
+    }
+    qtd--
+    textoQtd.textContent = qtd
+    qtdHidden.value = qtd 
+}
+
+btnMais.addEventListener('click', aumentar)
+btnMenos.addEventListener('click', diminuir)
+//====fim contator usando input hidden====
+
+//====inicio validação com regex campo nome====
+let inputNome = document.querySelector('#inputNome')
+inputNome.addEventListener('blur', function() {
+    let regexTexto = /[A-Z][a-z]* [A-Z][a-z]*/
+
+    // if (this.value.trim() == "") { sem regex
+    if (regexTexto.test(this.value.trim())) {
+        document.querySelector('.erro-nome').style.display = 'none'
+        document.querySelector('.sucesso-nome').style.display = 'block'
+    } else {
+        document.querySelector('.sucesso-nome').style.display = 'none'
+        document.querySelector('.erro-nome').style.display = 'block'
+    }
+})
+//====fim validação campo nome====
+
+//====inicio validação com regex campo Email====
+let inputEmail = document.querySelector('#inputEmail')
+inputEmail.addEventListener('blur', function() {
+    let regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+    if (regexEmail.test(inputEmail.value.trim())) {
+        document.querySelector('.erro-email').style.display = 'none'
+        document.querySelector('.sucesso-email').style.display = 'block'
+    } else {
+        document.querySelector('.sucesso-email').style.display = 'none'
+        document.querySelector('.erro-email').style.display = 'block'
+    }
+})
+//====fim validação campo Email====
+
+
+//====incio testes de eventos com o campo nome declarado na linha 29====
+inputNome.addEventListener('focus', function() {
+    // console.log('evento focus')
+})
+
+inputNome.addEventListener('blur', function() {
+    // console.log('evento blur')
+})
+
+inputNome.addEventListener('change', function() {
+    // console.log('evento change')
+})
+
+inputNome.addEventListener('input', function() {
+    // console.log('evento input')
+})
+//====fim testes de eventos com o campo nome declarado na linha 29====
+
+//====incio teste de captura de mudança em um select====
+let selectEstado = document.querySelector('.select-custom')
+selectEstado.addEventListener('change', function() {
+    let selectedIndex = selectEstado.selectedIndex
+    let itemSelected = selectEstado.options[selectedIndex]
+    // console.log(itemSelected.value)
+})
+//====fim teste de captura de mudança em um select====
+
+
+
+    
+//====incio teste do evento que enviar um formulario====
+let form = document.querySelector('#form-aula')
+form.addEventListener('submit', function(event) {
+    event.preventDefault() //cancela o comportamento padrão de enviar uma action
+
+    //selecionar o radio que está checado
+    let radio = document.querySelector('.radio-custom:checked')
+    // console.log(radio.value)
+
+    //seleciona todos os checkbox com a classe "aceite-promocoes"
+    let aceitePromo = document.querySelectorAll('.aceite-promocoes')
+    // console.log(aceitePromo)
+
+    //percorre os checkbox e verifica qual está checado, criando um novo array
+    aceiteChecados = []
+    for (let i = 0; i < aceitePromo.length; i++) {
+        if (aceitePromo[i].checked == true) {
+            //momento em que adicionamos um item checado no final do array
+            aceiteChecados.push(aceitePromo[i].value)
+        }
+    }
+    //o código abaixo faz a mesma coisa que o for acima de maneira diferente
+    // aceitePromo.forEach(value => {
+    //     if (value.checked == true) {
+    //         aceiteChecados.push(value.value)
+    //     }
+    // })
+
+    // console.log(aceiteChecados)
+
+    let textArea = document.querySelector('#exampleFormControlTextarea1')
+    // console.log(textArea.value)
+    
+})
+//====fim teste do evento que enviar um formulario====
